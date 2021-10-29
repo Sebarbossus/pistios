5bc7184afa1a1ab46a7e3c5cc90ad24772c9a335
AlvarezAcebal <nicolas.alvarez-acebal@cbc.de>
2021-10-29 15:13:30 +0200

    feat: add disqus url

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -3,7 +3,7 @@ import { DiscussionEmbed } from "disqus-react";
 const DisqusComments = ({ article }) => {
   const disqusShortname = "pistios-com";
   const disqusConfig = {
-    url: article.slug,
+    url: `https://www.pistios.com/article/${article.slug}`,
     identifier: article.slug, // Single post id
     title: article.title, // Single post title
   };
