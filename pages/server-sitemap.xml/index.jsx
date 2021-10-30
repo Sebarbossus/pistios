4057e466897b6812669e2878ba8047d2894ad0d4
AlvarezAcebal <nicolas.alvarez-acebal@cbc.de>
2021-10-30 15:28:46 +0200

    fix: webpack error 4

--- pages/server-sitemap.xml/index.jsx
+++ pages/server-sitemap.xml/index.jsx
@@ -1,10 +1,1 @@
-import { GetServerSideProps } from "next";
-import { getServerSideSitemap } from "next-sitemap";
-
-export const getServerSideProps = async (ctx) => {
-  const fields = [];
-
-  return getServerSideSitemap(ctx, fields);
-};
-
 export default function Sitemap() {}
